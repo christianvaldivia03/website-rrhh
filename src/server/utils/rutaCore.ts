d57a858e35a981198a6ea1a517d12c07f7e4537b
chriskira03 <christian.valdivia@unmsm.edu.pe>
2024-02-18 22:28:04 -0500

    Add rutaCore and rutaRrhh utility files

---
+++ src/server/utils/rutaCore.ts
@@ -0,0 +1,4 @@
+export const rutaCore = {
+  data_planilla: "mantenimiento/search-type-planilla",
+  data_lista: "mantenimiento/search-list",
+};
---
+++ src/server/utils/rutaRrhh.ts
@@ -0,0 +1,10 @@
+import { Search } from "semantic-ui-react";
+const planilla = "planilla/";
+const trabajador = "trabajador";
+
+export const rutaRrhh = {
+  create_planilla: `${planilla}create`,
+  search_list_planilla: `${planilla}search-list`,
+  search_one_planilla: `${planilla}search-one`,
+  search_concepto_planilla_trabajador: `${planilla}search-concepto`,
+};
